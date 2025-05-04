f93a5490838a068868856956fce8480771723e00
csedge <ejlindayao@gmail.com>
2025-05-04 23:29:30 +0800

    admin jobseeker statistics change args instead of json

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1678,9 +1678,8 @@ def get_hired_applicants():
 def get_jobseeker_statistics():
     try:
 
-        data = request.get_json()
-        start_date = data.get('start_date')
-        end_date = data.get('end_date')
+        start_date = request.args.get('start_date')
+        end_date = request.args.get('end_date')
 
         # Initialize base query with User and PersonalInformation
         base_query = User.query.join(PersonalInformation).filter(User.user_type == 'JOBSEEKER')
